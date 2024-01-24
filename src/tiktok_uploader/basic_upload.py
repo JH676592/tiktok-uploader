4649779c7ff903856610580e478939fdc76144fd
JH676592 <jacksonholcomb01@gmail.com>
2024-01-24 13:55:26 -0600

    Update basic_upload.py

--- src/tiktok_uploader/basic_upload.py
+++ src/tiktok_uploader/basic_upload.py
@@ -7,10 +7,12 @@ import upload
 FILENAME = "upload.mp4"
 
 myList = ["cookies8.txt", "cookies7.txt"]
+sessionIDS = ['80a2658c101a4694968fce71a8cdbcd5', '7db6bf898ad8d4edc52a4963713cc41e']
 
 if __name__ == "__main__":
 
     # upload video to TikTok
-    for cookieFile in myList:
+    for ID in myList:
+        
         upload.upload_video(FILENAME,
-                    description="This is a video I just downloaded", cookies=cookieFile)
+                    description="This is a video I just downloaded", cookies="cookies8.txt")
